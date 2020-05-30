0645dad36d013d8db4324adb89735255333663d3
Florian Waibel <fwaibel@datenkollektiv.de>
2020-05-31 00:16:23 +0200

    Switch from plain webpack 3.x to Angular CLI (webpack 4.x)

--- src/main.browser.ts
+++
@@ -1,8 +0,0 @@
-import 'core-js/es6';
-import 'core-js/es7/reflect';
-import 'zone.js/dist/zone';
-
-import {HomepageModule} from './app/homepage.module';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-
-platformBrowserDynamic().bootstrapModule(HomepageModule);
