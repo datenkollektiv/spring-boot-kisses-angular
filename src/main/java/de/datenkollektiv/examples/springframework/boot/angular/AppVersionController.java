bf20bd072038b21284ebb020966d2b7088c21664
Florian Waibel <fwaibel@datenkollektiv.de>
2021-05-22 10:30:34 +0200

    Bump Angular from 9.1.9 to 10.2.5
    Also update Spring Boot from 2.3.0 to 2.4.5 and
    Node from 12.17.0 to 14.17.0

--- src/main/java/de/datenkollektiv/examples/springframework/boot/angular/AppVersionController.java
+++ src/main/java/de/datenkollektiv/examples/springframework/boot/angular/AppVersionController.java
@@ -1,5 +1,7 @@
 package de.datenkollektiv.examples.springframework.boot.angular;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/server/*")
 public class AppVersionController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(AppVersionController.class);
+
     @GetMapping("version")
     public @ResponseBody
     AppVersion version() {
+        LOG.info("Serving 'version' request");
         return new AppVersion().withNumber("42");
     }
 }
