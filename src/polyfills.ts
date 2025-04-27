d4b2c433186588a061bda4da49d6ce7da9e3afea
Florian Waibel <fwaibel@datenkollektiv.de>
2025-04-27 22:36:45 +0200

    Angular v17 update

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -2,7 +2,7 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'core-js/features/reflect'
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
