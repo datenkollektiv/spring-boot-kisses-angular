3eb65f3d312b852abcce22e5900d90c137662898
Florian Waibel <fwaibel@datenkollektiv.de>
2026-01-18 22:43:34 +0100

    Angular 17 → 18 core packages upgrade

--- src/app/appVersion.service.ts
+++ src/app/appVersion.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 import {AppVersion} from "./model/appVersion";
 import {Observable} from 'rxjs';
--- src/app/homepage.module.ts
+++ src/app/homepage.module.ts
@@ -1,13 +1,10 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import {AppVersionComponent} from './appVersion.component';
 
-@NgModule({
-    declarations: [AppVersionComponent],
-    imports: [BrowserModule, HttpClientModule],
-    bootstrap: [AppVersionComponent]
-})
+@NgModule({ declarations: [AppVersionComponent],
+    bootstrap: [AppVersionComponent], imports: [BrowserModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class HomepageModule {
 }
